8e071f521cece994b0374f330b8535785bf9e887
Daniel Wachtel <daniel.wachtel@allori.edu.au>
2020-09-13 10:19:49 +1000

    Refactor toy robot spec

--- spec/toy_robot_spec.rb
+++ spec/toy_robot_spec.rb
@@ -1,29 +1,27 @@
 RSpec.describe ToyRobot do
-  it "#placed? returns false before robot is placed" do
-    robot = ToyRobot.new
+  let(:placement) { Placement.new(x: 2, y: 0, direction: "NORTH") }
 
-    expect(robot.placed?).to be false
+  before(:each) do
+    @robot = ToyRobot.new
+  end
+
+  it "#placed? returns false before robot is placed" do
+    expect(@robot.placed?).to be false
   end
 
   it "#placed? returns true after robot is placed" do
-    placement = Placement.new(x: 2, y: 0, direction: "NORTH")
-    robot = ToyRobot.new
-    robot.set_placement!(placement)
+    @robot.set_placement!(placement)
 
-    expect(robot.placed?).to be true
+    expect(@robot.placed?).to be true
   end
 
   it "#report returns formatted string if robot placed" do
-    placement = Placement.new(x: 2, y: 0, direction: "NORTH")
-    robot = ToyRobot.new
-    robot.set_placement!(placement)
+    @robot.set_placement!(placement)
 
-    expect(robot.report).to eq "2,0,NORTH"
+    expect(@robot.report).to eq "2,0,NORTH"
   end
 
   it "#report returns nil if robot not placed" do
-    robot = ToyRobot.new
-
-    expect(robot.report).to be nil
+    expect(@robot.report).to be nil
   end
 end
