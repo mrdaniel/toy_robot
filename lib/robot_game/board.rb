86ac9b0024bbb9b226953488efcf39669b000184
Daniel Wachtel <daniel.wachtel@allori.edu.au>
2020-09-13 16:53:21 +1000

    Update Board#to_s method

--- lib/robot_game/board.rb
+++ lib/robot_game/board.rb
@@ -6,6 +6,10 @@ class Board
     @height = 0..4
   end
 
+  def to_s
+    "#{@width.size}x#{@height.size} board"
+  end
+
   def coordinates_valid?(placement)
     @width.include?(placement.x) && @height.include?(placement.y)
   end
